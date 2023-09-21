9e2cb3fc8577a5cf1764301fa0166e0a3aeeabbf
Bruno Miglioretto <brunomiglioretto@gmail.com>
2023-09-21 18:58:41 -0300

    initial commit

---
+++ services/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy_utils import database_exists, create_database
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+instance = f"mysql+pymysql://root:vBst&Ebb5hw@localhost:3306/psychiatric_hospital"
+
+if not database_exists(url=instance):
+    create_database(url=instance)
+
+engine = create_engine(url=instance, echo=True)
+session = Session(bind=engine, autocommit=False, autoflush=True)
